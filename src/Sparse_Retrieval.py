56c03f68b1ed89f444e0d54089c33161406d62a6
rejasupotaro <takiguchi0817@gmail.com>
2023-05-04 08:56:47 +0900

    Display aggregated results

--- src/Sparse_Retrieval.py
+++ src/Sparse_Retrieval.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from typing import DefaultDict
+
 import pandas as pd
 import streamlit as st
 
@@ -10,20 +13,34 @@ def load_products() -> pd.DataFrame:
 def main():
     st.write("## Sparse Retrieval")
     products_df = load_products()
+    fields = ["product_title", "product_brand", "product_color", "product_bullet_point"]
+    default_weights = [1.0, 0.6, 0.4, 0.2]
 
     st.write("### Input")
-    query = st.text_input("query")
-    query_tokens = query.split()
+    with st.form("input"):
+        query = st.text_input("query")
+        query_tokens = query.split()
+
+        weight_cols = st.columns(len(fields))
+        weights = []
+        for i, (field, default_weight) in enumerate(zip(fields, default_weights)):
+            with weight_cols[i]:
+                field_name = " ".join(field.split("_")[1:])
+                weight = st.number_input(f"{field_name} weight", value=default_weight)
+                weights.append(weight)
+
+        aggregation = st.checkbox("aggregation", value=True)
+
+        submitted = st.form_submit_button("search")
+        if not submitted:
+            return
 
     st.write("### Results")
     product_dicts = products_df.to_records("records")
     top_k = 10
-    fields = ["product_title", "product_brand", "product_color", "product_bullet_point"]
-    columns = st.columns(len(fields))
-    for column, field in zip(columns, fields):
-        with column:
-            field_name = " ".join(field.split("_")[1:])
-            st.write(f"#### {field_name}")
+    if aggregation:
+        candidates: DefaultDict[str, list[float]] = defaultdict(list)
+        for i, field in enumerate(fields):
             products = [
                 product
                 for product in product_dicts
@@ -31,13 +48,37 @@ def main():
             ]
             for product in products[:top_k]:
                 product_id = product["product_id"]
-                text = product[field]
-                score = 1.0
+                product_title = product["product_title"]
+                score = 1.0 * weights[i]
+                candidates[(product_id, product_title)].append(score)
 
-                st.markdown(f"ID: {product_id}, score: {score}")
-                if not pd.isnull(text):
-                    st.markdown(text, unsafe_allow_html=True)
-                st.markdown("----")
+        sorted_candidates = sorted(candidates.items(), key=lambda id_and_score: sum(id_and_score[1]), reverse=True)
+        for (product_id, product_title), scores in sorted_candidates[:top_k]:
+            score = sum(scores)
+            score_text = " + ".join([str(s) for s in scores])
+            st.markdown(f"ID: {product_id}, score: {score} ({score_text})")
+            st.markdown(product_title)
+            st.markdown("----")
+    else:
+        columns = st.columns(len(fields))
+        for i, (column, field) in enumerate(zip(columns, fields)):
+            with column:
+                field_name = " ".join(field.split("_")[1:])
+                st.write(f"#### {field_name}")
+                products = [
+                    product
+                    for product in product_dicts
+                    if any(query_token in str(product[field]) for query_token in query_tokens)
+                ]
+                for product in products[:top_k]:
+                    product_id = product["product_id"]
+                    score = 1.0 * weights[i]
+                    st.markdown(f"ID: {product_id}, score: {score}")
+
+                    text = product[field]
+                    if not pd.isnull(text):
+                        st.markdown(text, unsafe_allow_html=True)
+                    st.markdown("----")
 
 
 if __name__ == "__main__":

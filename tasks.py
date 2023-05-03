91d9bd954dd850fa34d884b09f99287211629f52
rejasupotaro <takiguchi0817@gmail.com>
2023-05-03 14:44:20 +0900

    Encode all the titles

--- tasks.py
+++ tasks.py
@@ -8,17 +8,20 @@ from invoke import task
 
 @task
 def encode(c):
-    products_df = pd.read_csv("data/products_small_jp.csv.zip").head(100)
+    products_df = pd.read_csv("data/products_small_jp.csv.zip")
     products = products_df.to_dict("records")
     encoder = BERTEncoder(bert_model_name="ku-nlp/deberta-v2-base-japanese")
-    product_ids = [product["product_id"] for product in products]
-    product_titles = [product["product_title"] for product in products]
-    title_embs = encoder.encode(product_titles)
 
+    product_ids = [product["product_id"] for product in products]
     with open("data/product_ids.pkl", "wb") as file:
         pickle.dump(product_ids, file)
+        print("product_ids.pkl was saved.")
+
+    product_titles = [product["product_title"] for product in products]
+    title_embs = encoder.encode(product_titles)
     with open("data/title_embs.npy", "wb") as file:
         np.save(file, title_embs)
+        print("title_embs.npy was saved.")
 
 
 @task

375e483d22fa325246cd269e451eb51baab04396
rejasupotaro <takiguchi0817@gmail.com>
2023-05-07 14:05:27 +0900

    Remove isort, black, and flake8

--- tasks.py
+++ tasks.py
@@ -35,31 +35,3 @@ def app(c):
 @task
 def export_dependencies(c):
     c.run("poetry export --without-hashes --format=requirements.txt > requirements.txt")
-
-
-@task
-def format(c):
-    """Run formatters (isort and black)."""
-    print("Running isort...")
-    c.run("poetry run isort .")
-
-    print("Running black...")
-    c.run("poetry run black .")
-    print("Done")
-
-
-@task
-def lint(c):
-    """Run linters (isort, black, flake8, and mypy)."""
-    print("Running isort...")
-    c.run("poetry run isort . --check")
-
-    print("Running black...")
-    c.run("poetry run black . --check")
-
-    print("Running flake8...")
-    c.run("poetry run pflake8 src tests")
-
-    print("Running mypy...")
-    c.run("poetry run mypy src")
-    print("Done")
